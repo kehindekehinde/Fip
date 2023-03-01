20b2edb47ffc0f3b0091bdc999f480820d75649c
kehindekehinde <60239853+kehindekehinde@users.noreply.github.com>
2023-03-01 08:11:25 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// var fun = 5;
+
+
+// function funFunction() {
+//   var fun = "helloo";
+//   console.log(1, fun);
+// }
+
+// function funerFunction() {
+//   var fun = "byeee";
+//   console.log(2, fun);
+// }
+
+// function funnestFunction() {
+//   var fun = "AHHHHHH";
+//   console.log(3, fun);
+// }
+// console.log("window", fun);
+
+// funFunction();
+// funerFunction();
+// funnestFunction();
+
+// let and const
+
+// const player='bobby';
+// let experience= 100;
+// var wizardLevel = false;
+
+// if(experience =90){
+//     var wizardLevel= true
+// }
+
+// const player = "bobby";
+// let experience = 100;
+// let wizardLevel = false;
+
+// if ((experience = 90)) {
+//   let wizardLevel = true;
+//   console.log("inside", wizardLevel);
+// }
+// console.log("outside", wizardLevel);
+
+// const obj = {
+//   player: "bobby",
+//   experience: 100,
+//   wizardLevel: false,
+// };
+
+// const player = obj.player;
+// const experience = obj.experience;
+// let wizardLevel = obj.wizardLevel;
+
+// const { player, experience } = obj;
